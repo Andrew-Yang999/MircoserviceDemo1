7c5fd361815c340e9ade6e21c7671e1e411c1fd7
杨胜 <yangsheng@xforceplus.com>
2021-08-08 17:55:44 +0800

    添加config 相关的代码模块

---
+++ Cloud-config-client-19004/src/main/java/com/andrew/ConfigClientMain19004.java
@@ -0,0 +1,20 @@
+package com.andrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author Andrew Yang
+ * @ClassName ConfigCenterMain19003
+ * @Description
+ * @Package com.andrew
+ * @Create 2021-08-08 16:17
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain19004 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain19004.class, args);
+    }
+}
